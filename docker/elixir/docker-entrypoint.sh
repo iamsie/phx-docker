dc9bd94f0a5f947d43d4c81570f45ed3f1bf3127
Sierra <iamsi2@protonmail.com>
2020-05-22 08:20:03 +0300

    phoenix docker file

---
+++ docker/elixir/docker-entrypoint.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# fail if we error out
+set "MIX_ENV=dev" && mix compile
+# Wait for database to come up
+# until psql -h db -U "postgres" -c '\q' 2>/dev/null; do
+#   >&2 echo "Postgres is unavailable - sleeping"
+#   sleep 1
+# done
+# Ensure we have the basics...
+mix local.hex --force
+mix local.rebar --force
+mix deps.clean mime --build
+mix deps.get
+mix ecto.create
+mix ecto.migrate
+mix phx.server
